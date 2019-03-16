a2a57c019b9204bcc398ae78b619fe715cf31085
Omid Nasri <omid.nasri@outlook.com>
2019-03-16 23:24:26 +0330

    Users need access to remove public WF

--- src/Home.js
+++ src/Home.js
@@ -15,53 +15,72 @@ class Home extends Component {
     });
   }
 
+  deleteWorkflow = (event) => {
+    var id = event.target.getAttribute("data-id");
+    window.fetch("/admin/workflows/delete", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ id: id })}).then(response => {
+      if (response.status == 200)
+        this.setState({ dataSource: this.state.dataSource.filter(function (elem) { return elem.Id != id; }) });
+      else
+        console.log(response);
+    });
+  }
+
   renderTable(props) {
     if (props.parent.state.dataSource.length === 0) {
       return null;
     }
     else {
       return (
-        <table className="table table-striped table-bordered table-hover">
-          <thead>
-            <tr>
-              <th>ردیف</th>
-              <th>عنوان</th>
-              <th>مالک چرخه</th>
-              <th>دسترسی</th>
-              <th>وضعیت</th>
-              <th>تعداد گردش کار در حال اجرا</th>
-              <th data-res="false"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              props.parent.state.dataSource.map((item, index) => {
-                return (
-                  <tr>
-                    <td>{index + 1}</td>
-                    <td>{item.Subject}</td>
-                    <td>{item.Owner}</td>
-                    <td>{item.Access ? "عمومی" : "مخفی"}</td>
-                    <td>{item.Status ? "فعال" : "غیرفعال"}</td>
-                    <td>{item.WorkflowInprogressCount}</td>
-                    <td data-res="false">
-                      <a href={`/Admin/Workflows/Edit/${item.Id}`}> شخصی سازی </a> |
-                      <a href={`/Admin/Workflows/EditProperties/${item.Id}?ReturnUrl=/admin/workflows`}> ویرایش </a> |
-                      <a href={`/Admin/Workflows/Delete/${item.Id}`}> حذف </a>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="alert alert-warning">
+              <p>
+                گام سوم، به منظور راه‌اندازی و مدیریت مانیتورینگ سرور و یا سایت، کافیست چرخه‌کاری جدید ایجاد و فرآیندها را به هم متصل کنید.
+              </p>
+            </div>
+            <table className="table table-striped table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th>ردیف</th>
+                  <th>عنوان</th>
+                  <th>مالک چرخه</th>
+                  <th>دسترسی</th>
+                  <th>وضعیت</th>
+                  <th>تعداد گردش کار در حال اجرا</th>
+                  <th data-res="false"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  props.parent.state.dataSource.map((item, index) => {
+                    return (
+                      <tr>
+                        <td>{index + 1}</td>
+                        <td>{item.Subject}</td>
+                        <td>{item.Owner}</td>
+                        <td>{item.Access ? "عمومی" : "مخفی"}</td>
+                        <td>{item.Status ? "فعال" : "غیرفعال"}</td>
+                        <td>{item.WorkflowInprogressCount}</td>
+                        <td data-res="false">
+                          <a href={`/Admin/Workflows/Edit/${item.Id}`}> شخصی سازی </a> |
+                          <a href={`/Admin/Workflows/EditProperties/${item.Id}?ReturnUrl=/admin/workflows`}> ویرایش </a> |
+                          <a data-id={item.Id} onClick={item.Access ? `` : (event) => props.onDeleteWorkflow(event)} className={`${item.Access ? "disabled-link" : ""}`}> حذف </a>
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
       );
     }
   }
 
   render() {
     return (
-      <this.renderTable parent={this} />
+      <this.renderTable parent={this} onDeleteWorkflow={this.deleteWorkflow} />
     );
   }
 }
